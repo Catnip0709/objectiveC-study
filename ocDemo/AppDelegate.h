15f67311b923ecaf286f070ea8402f0ca2b12aff
panqiaoqiao <panqiaoqiao.catnip@bytedance.com>
2020-08-04 11:50:38 +0800

    Initial Commit

---
+++ ocDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ocDemo
+//
+//  Created by catnip on 2020/8/4.
+//  Copyright © 2020 catnip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ocDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ocDemo
+//
+//  Created by catnip on 2020/8/4.
+//  Copyright © 2020 catnip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
